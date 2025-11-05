e98437ba976ac1070a2a5aed5539b97e773c7f17
Partho2006 <parthojotyroychowdhury@gmail.com>
2025-11-05 18:54:49 +0530

    4th

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 <body>
     <?php 
     echo "Hello World!";
-    /* bitch 
-    yes you are a bi
+    /* gunna say 
+    yes you will follow me
     */
     //also son of a bi
     ?>
@@ -18,16 +18,16 @@
     <p>son of a bitch !!</p> 
     <?php } ?>
 
-    <p>this is a Paragraph, <?php echo "bitch" ?></p>
+    <p>this is a Paragraph, <?php echo "Hello" ?></p>
 
     <?php //naming convention
-    $name = "fu*king bitch";
+    $name = "Partho";
     echo $name;
-    echo "  You are a " . $name;
+    echo ",,,  I am " . $name;
     ?>
 
     <?php 
-    // variables
+    // variables-- Scaler type(one value)
     $string = "bitch";
     $int = 67;
     $float = 6.7;
@@ -35,6 +35,21 @@
 
     // array
     $array = ["you", "are a", "bitch"]
+    ?>
+
+    <p>Hi I am <?php echo "$name" ?></p>
+
+    <?php 
+    // global variables(gives name of the folder)
+    echo $_SERVER["DOCUMENT_ROOT"];
+    echo "<br>";
+    echo $__SERVER["SERVER_NAME"];
+    echo "<br>";
+    ?>
+
+    <?php 
+    // get method
+    echo $_GET["name"] //this will work for localhost/webphp(folder)/index.php?name=partho  --it gets value from the net
     ?>
 </body>
 </html>
