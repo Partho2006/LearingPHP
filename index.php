862f021089bbec72e0741326a49e3467864242f8
Partho2006 <parthojotyroychowdhury@gmail.com>
2025-11-05 17:45:31 +0530

    3rd

--- index.php
+++ index.php
@@ -8,6 +8,33 @@
 <body>
     <?php 
     echo "Hello World!";
+    /* bitch 
+    yes you are a bi
+    */
+    //also son of a bi
+    ?>
+
+    <?php if (true) {?>
+    <p>son of a bitch !!</p> 
+    <?php } ?>
+
+    <p>this is a Paragraph, <?php echo "bitch" ?></p>
+
+    <?php //naming convention
+    $name = "fu*king bitch";
+    echo $name;
+    echo "  You are a " . $name;
+    ?>
+
+    <?php 
+    // variables
+    $string = "bitch";
+    $int = 67;
+    $float = 6.7;
+    $bool = false;
+
+    // array
+    $array = ["you", "are a", "bitch"]
     ?>
 </body>
 </html>
